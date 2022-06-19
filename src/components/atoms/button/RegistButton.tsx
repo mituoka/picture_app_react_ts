6bb995d23c998a7580db392bcce0c94ee13371cc
MT <mitutumi0210@gmail.com>
2022-06-20 03:40:11 +0900

    first commit

---
+++ src/components/atoms/button/RegistButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export const Regist = () =>{
+    return (
+    <>
+      <Button variant="contained">登録</Button>
+    </>
+  );
+};
+
---
+++ src/components/atoms/button/SidebarButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export const SidebarElement  = () =>{
+    return (
+    <>
+      <Button variant="contained" disableElevation>
+        Disable elevation
+      </Button>
+    </>
+  );
+};
+
+
